851ae54e3ecf98c17017d13553d5ffec35f6487b
Jesse Woody <gearsighted@gmail.com>
2021-07-05 21:37:00 -0400

    Header rendering and Tailwind CSS successfully added

--- react-app/site-transfer/src/index.js
+++ react-app/site-transfer/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Header />
   </React.StrictMode>,
+
   document.getElementById('root')
 );
 
