6db6c331f8bfe98bf07788bdea2cdb855e634449
gearsighted <gearsighted@DESKTOP-GPNCFRS.localdomain>
2021-08-03 20:34:14 -0400

    fixing some spacing in about site

--- react-app/site-transfer/src/Components/AboutSite.jsx
+++ react-app/site-transfer/src/Components/AboutSite.jsx
@@ -12,10 +12,10 @@ function AboutSite() {
           />
         </div>{" "}
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 className="title-font sm:text-4xl text-3xl mb-5 ml-5 font-medium max-w-lg">
+          <h1 className="title-font sm:text-4xl text-3xl mb-5 ml-10 font-medium max-w-lg">
             About This Site:
           </h1>{" "}
-          <p className="ml-5 leading-relaxed mb-5 max-w-lg">
+          <p className="ml-10 leading-relaxed mb-5 max-w-lg">
             This website isn't only my portfolio, but also a place to experiment with, or at least post content about,
             any of the cool tech I'm learning. I try to keep the important parts fully functional, but it's hard 
             to make any promises when you're hacking together something behind the scenes. If you see this and have any 
@@ -24,7 +24,7 @@ function AboutSite() {
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="ml-4 inline-flex bg-brand-warning border-0 py-2 px-4 text-black focus:outline-none hover:bg-brand-danger 
+              className="ml-10 inline-flex bg-brand-warning border-0 py-2 px-4 text-black focus:outline-none hover:bg-brand-danger 
               hover:text-white rounded text-lg "
             >
               Make A Suggestion
