704549691944387ce9451a4707db8042a46058fe
gearsighted <gearsighted@DESKTOP-GPNCFRS.localdomain>
2021-08-06 20:40:48 -0400

    fixed error caused by removing post image

--- react-app/site-transfer/src/Components/Post.jsx
+++ react-app/site-transfer/src/Components/Post.jsx
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../client.js";
 import BlockContent from "@sanity/block-content-to-react";
-import imageUrlBuilder from "@sanity/image-url";
+// import imageUrlBuilder from "@sanity/image-url";
 import PostNav from "./PostNav";
 
-const builder = imageUrlBuilder(sanityClient);
-function urlFor(source) {
-  return builder.image(source);
-}
+// const builder = imageUrlBuilder(sanityClient);
+// function urlFor(source) {
+//   return builder.image(source);
+// }
 
 function Post() {
   const [postData, setPostData] = useState(null);
